3b26944ffa547be7ddfb074009820f35c4e97f20
aadii770 <adityabarde9770@gmail.com>
2026-08-14 16:21:45 +0530

    Add root redirect to /api/docs/ and clean staticfiles config

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
+
+def root_redirect(request):
+    return redirect('/api/docs/')
+
+
 urlpatterns = [
+    path('', root_redirect, name='root-redirect'),
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
