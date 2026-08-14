cb740766160a76837521e74c1520bfa07cf4c25c
aadii770 <adityabarde9770@gmail.com>
2026-08-14 16:26:38 +0530

    Auto-detect Render environment in settings __init__.py to enforce production settings

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,8 +1,9 @@
 import os
 
-ENVIRONMENT = os.environ.get('DJANGO_ENVIRONMENT', 'development')
+ENVIRONMENT = os.environ.get('DJANGO_ENVIRONMENT', '').lower()
+IS_RENDER = os.environ.get('RENDER') == 'true' or 'RENDER_SERVICE_ID' in os.environ
 
-if ENVIRONMENT == 'production':
+if ENVIRONMENT == 'production' or IS_RENDER or os.environ.get('DJANGO_SETTINGS_MODULE', '').endswith('production'):
     from config.settings.production import *  # noqa
 else:
     from config.settings.development import *  # noqa
